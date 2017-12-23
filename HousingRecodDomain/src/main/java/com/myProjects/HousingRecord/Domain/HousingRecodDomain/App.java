b81cba3c2e599ad63f60bf39fa8ab6842dd522b6
Tommi Korteikko <Tommi Korteikko>
2017-12-23 22:45:48 +0200

    inital commit. The structure of the project

---
+++ HousingRecodDomain/src/main/java/com/myProjects/HousingRecord/Domain/HousingRecodDomain/App.java
@@ -0,0 +1,13 @@
+package com.myProjects.HousingRecord.Domain.HousingRecodDomain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
