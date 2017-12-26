b4bf860ef217ddf331b284fb4c2d73e04c569523
Tommi Korteikko <Tommi Korteikko>
2017-12-26 20:20:35 +0200

    Initial structure of UserServiceImpl

--- HousingRecordRepository/src/main/java/com/myProjects/HousingRecord/Repository/Service/impl/UserServiceImpl.java
+++ HousingRecordRepository/src/main/java/com/myProjects/HousingRecord/Repository/Service/impl/UserServiceImpl.java
@@ -2,35 +2,67 @@ package com.myProjects.HousingRecord.Repository.Service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.myProjects.HousingRecord.Domain.UserAddress;
 import com.myProjects.HousingRecord.Domain.UserInformation;
+import com.myProjects.HousingRecord.Repository.DAOIF.UserDao;
 import com.myProjects.HousingRecord.Repository.Service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired UserDao userDao;
+	
+	@Transactional
 	public UserInformation userLogIn(String userName, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		UserInformation userInfo = null;
+		
+		if (!userName.equals(null)|| !password.equals(null)) {
+			userInfo = userDao.userLogin(userName, password);
+		}
+		else {
+			userInfo = null;
+		}
+			
+		return userInfo;
 	}
-
+	@Transactional
 	public List<UserInformation> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<UserInformation> userLst = userDao.getAllUsers();
+		
+		return userLst;
 	}
 
 	public UserAddress getUser(String userName, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		UserAddress userAdr = null;
+		if (!userName.equals(null)|| !password.equals(null)) {
+			userAdr = userDao.getUser(userName, password);
+		}
+		
+		return userAdr;
 	}
 
 	public UserAddress addUser(UserAddress theUser) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		UserAddress userAddress = null;
+		if (theUser != null) {
+			userAddress = userDao.addUser(theUser);
+		}
+		return userAddress;	
 	}
 
 	public UserInformation removeUser(double userId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		UserInformation userInfo = null;
+		if(userId >= 0 ) {
+			userInfo = userDao.removeUser(userId);
+		}
+		return userInfo;
 	}
 
 }
