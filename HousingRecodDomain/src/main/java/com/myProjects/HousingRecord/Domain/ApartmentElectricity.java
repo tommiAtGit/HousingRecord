17209b242305637e83df6488df8884f88177c89d
Tommi Korteikko <Tommi Korteikko>
2017-12-29 16:33:18 +0200

    Fix to project setup and pom file

--- HousingRecodDomain/src/main/java/com/myProjects/HousingRecord/Domain/ApartmentElectricity.java
+++ HousingRecodDomain/src/main/java/com/myProjects/HousingRecord/Domain/ApartmentElectricity.java
@@ -7,6 +7,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -30,7 +31,7 @@ public class ApartmentElectricity implements Serializable {
 	}
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	public long getId() {
 		return id;
--- HousingRecordRepository/src/main/java/com/myProjects/HousingRecord/Repository/DAO/UserDAOImpl.java
+++ HousingRecordRepository/src/main/java/com/myProjects/HousingRecord/Repository/DAO/UserDAOImpl.java
@@ -2,19 +2,38 @@ package com.myProjects.HousingRecord.Repository.DAO;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.myProjects.HousingRecord.Domain.UserAddress;
 import com.myProjects.HousingRecord.Domain.UserInformation;
 import com.myProjects.HousingRecord.Repository.DAOIF.UserDao;
 
+@Transactional
 public class UserDAOImpl implements UserDao {
 
+	@PersistenceContext
+	private EntityManager em;
+	
 	public UserAddress addUser(UserAddress userAddress) {
-		// TODO Auto-generated method stub
-		return null;
+
+		try {
+			em.persist(userAddress);
+		}
+		catch(Exception ex) {
+			//Do some thing
+		}
+		
+		
+	    return userAddress;
+	    
 	}
 
 	public List<UserInformation> getAllUsers() {
 		// TODO Auto-generated method stub
+		  
 		return null;
 	}
 
